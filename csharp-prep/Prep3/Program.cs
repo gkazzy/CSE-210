da6fe87d0a2d427c40f7ddd3a3ded19947f36956
Grace <gracekazzy@gmail.com>
2024-09-19 16:49:14 -0600

    Completed Prep #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -14,6 +14,7 @@ class Program
         {
         Console.WriteLine("What is your guess? ");
         string guess = Console.ReadLine();
+        guessNumber = int.Parse(guess);
 
             if (guessNumber > magicNumber)
             {
