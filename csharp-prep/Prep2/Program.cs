5b14df5abde81c37f639831afdecc0f347438eb5
Grace <gracekazzy@gmail.com>
2024-09-19 09:14:51 -0600

    Completed C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        string letter = "Unknown";
+
+        int lastDigit = number % 10;
+        string gradeSign = "";
+    if (number < 93 && number >= 60)
+        {
+            if (lastDigit >= 7)
+            {
+                gradeSign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                gradeSign = "-";
+            }
+        }
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{gradeSign}.");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("You have passed this course!");
+        }
+        else if (number < 70)
+        {
+            Console.WriteLine("You did not pass this course.");
+        }
+
     }
 }
