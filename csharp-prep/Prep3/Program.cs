343460944bcf533d43e8a8ea63c795195d572837
Grace <gracekazzy@gmail.com>
2024-09-19 16:41:02 -0600

    Sending to GIT because errors are occuring

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1,101);
+
+        int guessNumber = 200;
+
+        while (guessNumber != magicNumber)
+        {
+        Console.WriteLine("What is your guess? ");
+        string guess = Console.ReadLine();
+
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+        }
     }
 }
